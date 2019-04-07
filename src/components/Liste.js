c856f3d6d47c335a7c63ddea59087766ea101c15
Macbook Pro <macbookpro@Macbook-MacBook-Pro.local>
2019-04-07 19:06:04 +0300

    list component to class

---
+++ src/components/Liste.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Liste extends Component{
+    render(){
+        return(
+            <View style={{ marginTop: 5}}>
+                <Text> Örnek Proje Liste Sayfası </Text>
+            </View>
+        )
+    }
+}
+
+export default Liste;
