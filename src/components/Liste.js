a60797ef11f9453b2c4ebefcc2f7e5e409504241
Macbook Pro <macbookpro@Macbook-MacBook-Pro.local>
2019-04-08 22:19:18 +0300

    add axios and network request

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class Liste extends Component{
+    state = {
+        data: []
+    };
+    componentWillMount(){
+        axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ data: response.data }));
+    }
+
+    componentDidMount(){
+        console.log('cdm');
+    }
+
+    renderData(){
+        return this.state.data.map(responseData => 
+            <Text>{ responseData.title }</Text>
+        );
+    }
+
     render(){
+        console.log('gelen data : ' + this.state);
         return(
             <View style={{ marginTop: 5}}>
-                <Text> Örnek Proje Liste Sayfası </Text>
+                { this.renderData() }
             </View>
         )
     }
